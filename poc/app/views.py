423b8166323d5a1ea1630b72216b69731fcb03ac
Anne Fleischer <anneflydev@gmail.com>
2015-11-26 21:49:34 +0100

    add example for multiple formsets on one page

--- poc/app/views.py
+++ poc/app/views.py
@@ -1,9 +1,37 @@
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import FormView
 
 from poc.app.forms import BookFormset
 
 
-class BookView(FormView):
-    template_name = 'books.html'
+class BookListView(FormView):
+    template_name = 'book-list.html'
     form_class = BookFormset
     success_url = '/'
+
+
+class BookListMultipleView(FormView):
+    template_name = 'book-list-multiple.html'
+    form_class = BookFormset
+    success_url = reverse_lazy('book-list-multiple')
+
+    def get_form(self, form_class=None):
+        if form_class is None:
+            form_class = self.get_form_class()
+
+        form_kwargs = self.get_form_kwargs()
+
+        form_kwargs.update({'prefix': 'form-1'})
+        form1 = form_class(**form_kwargs)
+
+        form_kwargs.update({'prefix': 'form-2'})
+        form2 = form_class(**form_kwargs)
+
+        return [form1, form2]
+
+    def post(self, request, *args, **kwargs):
+        forms = self.get_form()
+        if all([form.is_valid() for form in forms]):
+            return self.form_valid(forms)
+        else:
+            return self.form_invalid(forms)
--- poc/urls.py
+++ poc/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from poc.app.views import BookView
-
 urlpatterns = [
     url(r'^select2/', include('django_select2.urls')),
-    url(r'^$', BookView.as_view(), name='home'),
+    url(r'^', include('poc.app.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
